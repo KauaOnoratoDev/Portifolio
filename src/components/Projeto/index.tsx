2013ac7c3ba4c066bbce27a1f22f1448cf4215e0
Kauã Onorato <kauaonorato.dev@gmail.com>
2024-03-06 22:06:31 -0400

    criando a seção de projetos e o componente de projetos

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,13 @@
+import Paragrafo from '../paragrafo'
+import Titulo from '../titulo'
+import { Card, LinkBotao } from './styles'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJs</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
